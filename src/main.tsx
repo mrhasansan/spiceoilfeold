15c172809adfeea583935886b0697152bd92e6e9
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-10 21:36:00 +0700

    fix: setup shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { RootRoute } from "./routes/root";
-import { HomeRoute } from "./routes/home";
-import { ErrorPage } from "./routes/errorpage";
 
-const router = createBrowserRouter([{ path: "/", element: <RootRoute />, errorElement: <ErrorPage />, children: [{ path: "/", element: <HomeRoute /> }] }]);
+import { RootRoute } from "@/routes/root";
+import { HomeRoute } from "@/routes/home";
+import { ErrorPage } from "@/routes/error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootRoute />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomeRoute />,
+      },
+      // {
+      //   path: "/products",
+      //   element: <ProductsRoute />,
+      // },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
@@ -73,5 +68,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: ["tailwindcss-animate"],
+};
